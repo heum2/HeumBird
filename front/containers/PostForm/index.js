0bf33c7bef2ade4f794841141cdff3b32f10f568
JoEunheum <jwh6295@gmail.com>
2020-02-16 16:20:00 +0900

    Success PostForm Design

--- front/containers/PostForm/index.js
+++ front/containers/PostForm/index.js
@@ -1,13 +1,19 @@
 import React, { useState, memo, useCallback, useRef, useEffect } from 'react';
-import { Form, Button } from 'antd';
+import { Form, Button, Dropdown, Menu } from 'antd';
 import { useSelector, useDispatch } from 'react-redux';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faImage } from '@fortawesome/free-regular-svg-icons';
+import {
+  faSortDown,
+  faGlobeAsia,
+  faUserFriends,
+  faLock,
+} from '@fortawesome/free-solid-svg-icons';
 import Editor from 'draft-js-plugins-editor';
 import createHashtagPlugin from 'draft-js-hashtag-plugin';
-import { EditorState, convertFromRaw } from 'draft-js';
+import { EditorState, convertFromRaw, convertToRaw } from 'draft-js';
 
-import { Card, DraftEditor } from './style';
+import { Card } from './style';
 
 const hashtagPlugin = createHashtagPlugin();
 const plugins = [hashtagPlugin];
@@ -25,42 +31,56 @@ const emptyContentState = convertFromRaw({
 });
 
 const PostForm = memo(() => {
-  useEffect;
   const dispatch = useDispatch();
-  const [text, setText] = useState('');
   const [editorState, setEditorState] = useState(
     EditorState.createWithContent(emptyContentState),
   );
-  // const [testeditor, setTestEditor] = useState(createEditorStateWithText(text));
-  // const [placeholderState, setPlaceholderState] = useState('block');
+  const editor = useRef(null);
   const { isAddingPost } = useSelector(state => state.post);
   const imageInput = useRef();
-  const hashtagRegex = /\#([0-9a-zA-Z가-힣]*)\s/;
 
   const onSubmitForm = useCallback(e => {
     e.preventDefault();
   }, []);
 
-  const onChangeTextArea = useCallback(
-    e => {
-      const { innerText } = e.currentTarget;
-      if (innerText.length > 0) {
-        setPlaceholderState('none');
-      } else {
-        setPlaceholderState('block');
-      }
-
-      setText(innerText);
-      // if (value.match(hashtagRegex)) {
-      // } else {
-      // }
-    },
-    [text],
-  );
+  const renderContentAsRawJs = () => {
+    const contentState = editorState.getCurrentContent();
+    const raw = convertToRaw(contentState);
+    console.log(raw.blocks[0].text);
+    return JSON.stringify(raw, null, 2);
+  };
 
   const onChangeEditor = editorState => {
     setEditorState(editorState);
+    const contentState = editorState.getCurrentContent();
+    const raw = convertToRaw(contentState);
+    console.log(raw.blocks[0].text.length);
   };
+
+  const focus = useCallback(() => {
+    if (editor.current) editor.current.focus();
+  }, []);
+
+  const onClickMenu = useCallback(({ key }) => {
+    console.log('click', key);
+  }, []);
+
+  const menu = (
+    <Menu onClick={onClickMenu}>
+      <Menu.Item key="2">
+        <FontAwesomeIcon icon={faGlobeAsia} />
+        전체 공개
+      </Menu.Item>
+      <Menu.Item key="1">
+        <FontAwesomeIcon icon={faUserFriends} />
+        팔로우들만
+      </Menu.Item>
+      <Menu.Item key="0">
+        <FontAwesomeIcon icon={faLock} />
+        나만 보기
+      </Menu.Item>
+    </Menu>
+  );
 
   const onChangeImages = useCallback(e => {}, []);
 
@@ -68,15 +88,70 @@ const PostForm = memo(() => {
 
   return (
     <>
-      {console.log('렌더링 확인!')}
-      {/* <DraftEditor> */}
-      <Editor
-        placeholder="여기다가 쓰세요!"
-        editorKey="foobaz"
-        editorState={editorState}
-        onChange={onChangeEditor}
-        plugins={plugins}
-      />
+      <div
+        style={{
+          background: 'white',
+          boxShadow: '0 4px 8px 0 rgba(0, 0, 0, 0.2)',
+          transition: '0.3s',
+          margin: '0px -1px 60px',
+          display: 'relative',
+          width: '100%',
+        }}
+      >
+        <Form>
+          <div style={{ padding: '5px' }}>
+            <Editor
+              placeholder="무슨 일이 일어나고 있나요?"
+              editorKey="foobaz"
+              editorState={editorState}
+              onChange={onChangeEditor}
+              plugins={plugins}
+              ref={editor}
+            />
+          </div>
+          <input
+            type="file"
+            multiple
+            hidden
+            ref={imageInput}
+            onChange={onChangeImages}
+          />
+          <div align="left" style={{ padding: 2, margin: 'auto' }}>
+            <Button type="link" onClick={onClickImageUpload} size="large">
+              <FontAwesomeIcon icon={faImage} size={'2x'} />
+            </Button>
+
+            <div
+              style={{
+                float: 'right',
+                marginRight: '1%',
+              }}
+            >
+              <Button
+                type="primary"
+                shape="round"
+                htmlType="submit"
+                loading={isAddingPost}
+              >
+                흐밋
+              </Button>
+            </div>
+            <div
+              style={{
+                float: 'right',
+                marginRight: '1%',
+              }}
+            >
+              <Dropdown overlay={menu}>
+                <Button>
+                  Button &nbsp;
+                  <FontAwesomeIcon icon={faSortDown} />
+                </Button>
+              </Dropdown>
+            </div>
+          </div>
+        </Form>
+      </div>
       {/* </DraftEditor> */}
       {/* <div
         style={{
