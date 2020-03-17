0482d83a947479e3d5c3cb26c4db8388433ea9b4
JoEunheum <jwh6295@gmail.com>
2020-03-17 21:15:56 +0900

    Add greenlock-express

--- back/index.js
+++ back/index.js
@@ -7,6 +7,8 @@ const dotenv = require("dotenv");
 const passport = require("passport");
 const hpp = require("hpp");
 const helmet = require("helmet");
+const https = require("https");
+const http = require("http");
 
 const passportConfig = require("./passport");
 const db = require("./models");
@@ -67,6 +69,29 @@ app.use("/api/user", userAPIRouter);
 app.use("/api/post", postAPIRouter);
 app.use("/api/posts", postsAPIRouter);
 app.use("/api/hashtag", hashtagAPIRouter);
-app.listen(prod ? process.env.PORT : 3060, () => {
-  console.log(`server is running on ${prod ? process.env.PORT : 3060}`);
-});
+
+if (prod) {
+  const lex = require("greenlock-express").create({
+    version: "draft-11",
+    configDir: "/etc/letsencrypt",
+    server: "https://acme-v02.api.letsencrypt.org/directory",
+    email: "jwh6295@gmail.com",
+    approveDomains: (opts, certs, cb) => {
+      if (certs) {
+        opts.domains = ["api.heumbird.com"];
+      } else {
+        opts.email = "jwh6295@gmail.com";
+        opts.agreeTos = true;
+      }
+      cb(null, { options: opts, certs });
+    },
+    renewWithin: 81 * 24 * 60 * 60 * 1000,
+    renewBy: 80 * 24 * 60 * 60 * 1000
+  });
+  https.createServer(lex.httpsOptions, lex.middleware(app)).listen(443);
+  http.createServer(lex.middleware(require("redirect-https")())).listen(80);
+} else {
+  app.listen(prod ? process.env.PORT : 3060, () => {
+    console.log(`server is running on ${prod ? process.env.PORT : 3060}`);
+  });
+}
